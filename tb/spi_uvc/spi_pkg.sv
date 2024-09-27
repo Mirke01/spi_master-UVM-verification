e47c39594d26ee57eede15e083e944284b9aec85
Mirke01 <147967357+Mirke01@users.noreply.github.com>
2024-09-27 10:40:25 +0200

    Create spi_pkg.sv

---
+++ tb/spi_uvc/spi_pkg.sv
@@ -0,0 +1,18 @@
+package spi_pkg;
+
+    import uvm_pkg::*;
+`include "spi_types.sv"
+`include "spi_defines.sv"
+`include "uvm_macros.svh"
+`include "spi_data_item.sv"
+`include "spi_config.sv"
+`include "spi_sequencer.sv"
+`include "spi_sequence_lib.sv"
+`include "spi_slave_driver.sv"
+`include "spi_master_driver.sv"
+`include "spi_monitor.sv"
+`include "spi_agent.sv"
+`include "spi_env.sv"
+
+
+endpackage
