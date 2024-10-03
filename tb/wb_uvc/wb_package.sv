3129ac3f6625b168c1c1a317437568fc93177a6f
Mirke01 <147967357+Mirke01@users.noreply.github.com>
2024-10-03 14:04:36 +0200

    Create wb_package.sv

---
+++ tb/wb_uvc/wb_package.sv
@@ -0,0 +1,19 @@
+package wb_package;
+  import uvm_pkg::*;
+  
+  `include "wb_define.sv"
+  `include "wb_types.sv"
+  `include "uvm_macros.svh"
+  `include "wb_config.sv"
+  `include "wb_seq_item.sv"
+  `include "wb_slave_monitor.sv"
+  `include "wb_slave_driver.sv"
+  `include "wb_slave_sequencer.sv"
+  `include "wb_slave_agent.sv"
+  `include "wb_master_monitor.sv"
+  `include "wb_master_driver.sv"
+  `include "wb_master_sequencer.sv"
+  `include "wb_master_agent.sv"
+  `include "wb_env.sv"
+  
+endpackage
